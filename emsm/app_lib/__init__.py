f78981850d2995d966a14679396d0a208b7ea894
Benedikt Schmitt <benedikt@benediktschmitt.de>
2014-09-01 00:53:35 +0200

    *cleaned up* userinput library

--- emsm/app_lib/__init__.py
+++ emsm/app_lib/__init__.py
@@ -1,2 +1,1 @@
 #!/usr/bin/env python3
-# Benedikt Schmitt <benedikt@benediktschmitt.de>
--- emsm/app_lib/userinput.py
+++ emsm/app_lib/userinput.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
-# Benedikt Schmitt <benedikt@benediktschmitt.de>
+
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org>
 
 
 """
-Some useful functions to get a valid input from the command line.
-
+This module provides some useful functions to get valid input from the
+command line.
 """
 
 
-__all__ = ["get_value", "ask"]
-
+# Functions
+# ------------------------------------------------
 
 def get_value(prompt, conv_func=None, check_func=None):
     """
-    Aks the value from the user.
+    Tries to get a value from the user.
 
     If only "prompt" is given, this function is equal to the
     built-in function "input".
@@ -45,25 +69,33 @@ def get_value(prompt, conv_func=None, check_func=None):
             break
     return temp
 
+
 def get_int(prompt, check_func=None):
+    """
+    """
     return get_value(prompt, int, check_func)
 
+
 def get_float(prompt, check_func=None):
+    """
+    """
     return get_value(prompt, float, check_func)
+
 
 def choose(prompt, choices):
     """
     The user has to choose an element of *choices*.
     """
-    # Build the whole prompt.
+    # We add a list of the coices to the prompt. The items
+    # index starts by 1.
     for i, choice in enumerate(choices):
-        prompt += "\n{}. {}".format(i, choice)
+        prompt += "\n{}. {}".format(i + 1, choice)
     prompt += "\n"
 
     # Let the user choose.
     while True:
         try:
-            index = int(input(prompt))
+            index = int(input(prompt)) - 1
             choice = choices[index]
         except KeyboardInterrupt:
             # This makes sure, that the user can still leave
@@ -79,9 +111,10 @@ def choose(prompt, choices):
             break
     return (index, choice)
 
+
 def ask(question, default=None):
     """
-    Asks the user the question until he answers it with yes or no.
+    Asks the user the question until he answers it with *yes* or *no*.
     
     Returns true if the user answered with yes.
     Returns *default* if the user only hit enter.
@@ -103,18 +136,3 @@ def ask(question, default=None):
             return True
         elif temp in ("N", "NO"):
             return False
-
-
-if __name__ == "__main__":
-    v = get_int("> an integer please")
-    print(v)
-    v = get_int("> an integer between 1 and 10", lambda i: 1 <= i <= 10)
-    print(v)
-
-    yes = ask("Do you like foo")
-    print(yes)
-    yes = ask("Do you like bar?", False)
-    print(yes)
-
-    choice = choose(["foo", "bar"])
-    print(choice)
