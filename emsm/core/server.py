b7e70e9e88a3945394cb3e7967d035fa0f2a144e
Steve Almond <steve@SilkAndSlug.com>
2020-11-20 14:31:13 +0000

    Update Forge to latest 1.12
    Signed-off-by: Steve Almond <steve@SilkAndSlug.com>

--- emsm/core/server.py
+++ emsm/core/server.py
@@ -933,7 +933,7 @@ class MinecraftForge_1_12(MinecraftForgeBase, Vanilla_1_12):
         return "minecraft forge 1.12"
 
     def default_url(self):
-        return "http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12-14.21.1.2405/forge-1.12-14.21.1.2405-installer.jar"
+        return "http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12-14.21.1.2443/forge-1.12-14.21.1.2443-installer.jar"
 
     def exe_path(self):
         filenames = [filename \
